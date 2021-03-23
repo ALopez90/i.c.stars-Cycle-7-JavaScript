4838c090e1375246e0b412c6f1f0027ab4a0b44b
ALopez90 <candiauralexalopez@gmail.com>
2021-03-22 20:58:11 -0500

    Updated HW file

--- homework/homework_322.js
+++ homework/homework_322.js
@@ -1,15 +1,16 @@
-// npm install prompt-sync
-var prompt = require('prompt-sync')();
+/**
+Homework: Fortune Teller
+Create a function that creates a furtune!
 
-let userName = prompt('What is your name?'),
-fortArray = ['Cow', 'Dog', 'Cat', 'Alien', 'Hamster'],
-ranFort = fortArray[Math.floor(Math.random * fortArray.length)],
-ranNum = Math.floor(Math.random * 100);
+Must:
+Takes in an array as a parameter
+Include an if/else conditional statement
+Returns a concatinated string of the user's fortune
 
-test(userName, ranFort, ranNum);
+Challenge: Takes in the user's input as a parameter
 
-function test(user, fortune, number) {
-  console.log(user);
-  console.log(fortune);
-  console.log(number);
-};
+DUE: 03/23/2021 @ 1pm.
+Deliver your HW by sharing your GitHub repo with me through email (alopez2@icstars.com)
+*/
+
+// you thought I'd just give the answer here? Smh.
